b56a6008aefdfadbb5a1ccae9d1a90248a03dbee
8LWXpg <105704427+8LWXpg@users.noreply.github.com>
2024-06-26 17:23:46 +0800

    update config

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,10 +1,5 @@
 return {
 	"nvim-neo-tree/neo-tree.nvim",
-	opt = {
-		filesystem = {
-			hijack_netrw_behavior = "open_default",
-		}
-	},
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
