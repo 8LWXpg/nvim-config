16868b7d75bb223d0207928ce13fa6979cda8a5b
8LWXpg <105704427+8LWXpg@users.noreply.github.com>
2024-06-27 14:00:36 +0800

    change starttify

--- lua/plugins/default.lua
+++ lua/plugins/default.lua
@@ -10,6 +10,21 @@ return {
 	'folke/neodev.nvim',
 	'honza/vim-snippets',
 	'nvim-treesitter/nvim-treesitter',
-	'mhinz/vim-startify',
 	{ 'ArcaneSpecs/HexEditor.nvim', config = true },
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+			"MunifTanjim/nui.nvim",
+		},
+		config = true,
+	},
+	{
+		'goolord/alpha-nvim',
+		dependencies = { 'nvim-tree/nvim-web-devicons' },
+		config = function()
+			require 'alpha'.setup(require 'alpha.themes.startify'.config)
+		end
+	},
 }
