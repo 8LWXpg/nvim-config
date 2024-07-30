fe6672d45573b7e080435f23010351298bf9b378
8LWXpg <105704427+8LWXpg@users.noreply.github.com>
2024-07-30 16:04:06 +0800

    fix telescope

--- lua/plugins/default.lua
+++ lua/plugins/default.lua
@@ -20,20 +20,7 @@ return {
 		'goolord/alpha-nvim',
 		dependencies = { 'nvim-tree/nvim-web-devicons' },
 		config = function()
-			require 'alpha'.setup(require 'alpha.themes.startify'.config)
-		end
-	},
-	{
-		'nvim-telescope/telescope.nvim',
-		dependencies = { 'nvim-lua/plenary.nvim' },
-		config = function(_, opts)
-			require('telescope').setup(opts)
-
-			local builtin = require('telescope.builtin')
-			vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-			vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-			vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-			vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+			require('alpha').setup(require 'alpha.themes.startify'.config)
 		end
 	},
 }
