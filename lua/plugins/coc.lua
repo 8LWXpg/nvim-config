578a78ac91babc921b86c344bcdb9a97de23a9e2
8LWXpg <105704427+8LWXpg@users.noreply.github.com>
2024-08-22 21:10:52 +0800

    fix coc version

--- lua/plugins/coc.lua
+++ lua/plugins/coc.lua
@@ -1,9 +1,4 @@
 return {
 	'neoclide/coc.nvim',
-	run = function()
-	-- Run `npm ci` after updating `coc.nvim`
-		local install_path = vim.fn.stdpath('data') .. '/lazy/coc.nvim'
-		vim.fn.system({'npm', 'ci'}, {cwd = install_path})
-	end,
+	version = false,
 }
-
