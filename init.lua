b1b3337852ce3954240130983a7318e2e776eb68
8LWXpg <105704427+8LWXpg@users.noreply.github.com>
2025-10-05 07:27:26 +0000

    fix osc52

--- init.lua
+++ init.lua
@@ -86,11 +86,20 @@ end
 
 -- Force OSC52 on remote
 if (os.getenv('SSH_TTY') ~= nil) then
+	local function vim_paste()
+		local content = vim.fn.getreg '"'
+		return vim.split(content, '\n')
+	end
+
 	vim.g.clipboard = {
 		name = 'OSC 52',
 		copy = {
 			['+'] = require('vim.ui.clipboard.osc52').copy('+'),
 			['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+		},
+		paste = {
+			['+'] = vim_paste,
+			['*'] = vim_paste,
 		},
 	}
 end
