82490158a6c425aa8cf505c8f7216f4deb7cf05b
8LWXpg <105704427+8LWXpg@users.noreply.github.com>
2025-10-06 11:13:08 +0000

    disable linenumber

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,3 +1,11 @@
+vim.api.nvim_create_autocmd('BufEnter', {
+	pattern = 'copilot-*',
+	callback = function()
+		vim.opt_local.relativenumber = false
+		vim.opt_local.number = false
+	end,
+})
+
 return {
 	{
 		'CopilotC-Nvim/CopilotChat.nvim',
@@ -8,8 +16,9 @@ return {
 		opts = {
 			model = 'claude-sonnet-4',
 			window = {
-				width = .3,
+				width = .4,
 			},
+			auto_fold = true,
 			auto_insert_mode = true,
 		},
 	},
