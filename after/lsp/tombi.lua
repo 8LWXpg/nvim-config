b21510011be6fe7e2a8038ad877e1ad39e2cffef
8LWXpg <105704427+8LWXpg@users.noreply.github.com>
2026-01-21 16:01:30 +0800

    add tombi

---
+++ after/lsp/tombi.lua
@@ -0,0 +1,12 @@
+return {
+	settings = {
+		tombi = {
+			format = {
+				rules = {
+					['string-quote-style'] = 'single',
+					['trailing-comment-alignment'] = true,
+				},
+			},
+		},
+	},
+}
