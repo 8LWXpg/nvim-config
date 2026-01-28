e7ee141cf997bdc785ddd7cdafb9d1b1fc6abb3f
8LWXpg <105704427+8LWXpg@users.noreply.github.com>
2026-01-28 21:59:24 +0800

    use Snacks.bufdelete

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -20,7 +20,7 @@ return {
 			show_buffer_close_icons = false,
 			enforce_regular_tabs = true,
 			right_mouse_command = false,
-			middle_mouse_command = 'bdelete! %d',
+			middle_mouse_command = 'lua Snacks.bufdelete.delete(%d)',
 		},
 	},
 	keys = {
