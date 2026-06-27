7f2fd8fec8ddb842a2b4d1be1875aa475b227bf0
8LWXpg <peng200208@gmail.com>
2026-06-27 16:56:24 +0800

    use fork before merge

--- lua/plugins/tree-sitter-manager.lua
+++ lua/plugins/tree-sitter-manager.lua
@@ -1,15 +1,5 @@
 return {
 	'romus204/tree-sitter-manager.nvim',
-	main = 'tree-sitter-manager',
 	event = { 'BufReadPost', 'BufNewFile' },
-	opts = {
-		languages = {
-			typst = {
-				install_info = {
-					url = 'https://github.com/uben0/tree-sitter-typst',
-					use_repo_queries = true,
-				},
-			},
-		},
-	},
+	opts = {},
 }
--- lua/plugins/typst-preview.lua
+++ lua/plugins/typst-preview.lua
@@ -1,5 +1,8 @@
 return {
-	'chomosuke/typst-preview.nvim',
+	-- 'chomosuke/typst-preview.nvim',
+	-- Partial rendering fork
+	'll-nick/typst-preview.nvim',
+	branch = 'partial-rendering-default',
 	ft = 'typst',
 	opts = {
 		dependencies_bin = { tinymist = 'tinymist.cmd' },
